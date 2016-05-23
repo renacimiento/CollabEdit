eca06033465bee6515b19ef0bd3cb67c6495380f
Parag <paragsaraogi@gmail.com>
2016-05-22 17:40:52 -0700

    Implement PPS & Fix bugs

--- PPS.py
+++ PPS.py
@@ -1,3 +1,5 @@
+import json
+
 class PPS:
 	ppsList = []
 	def getNewPosition(self,position):
@@ -7,7 +9,9 @@ class PPS:
 			return 1
 		elif position == len(self.ppsList) + 1:
 			return self.ppsList[len(self.ppsList)-1][0] + 1	
-		else: 
+		elif position==1:
+			return (float) (self.ppsList[position-1][0])/2
+		else:
 			return (float)(self.ppsList[position-2][0] + self.ppsList[position-1][0])/2
 
 	def insertCharacter(self,s,position):
@@ -16,24 +20,33 @@ class PPS:
 	def deleteCharacter(self,s,position):
 		del self.ppsList[position - 1]
 
-	def print_ppsList(self):
+	def print_pps(self):
 		for obj in self.ppsList:
 			print obj[1] + " " + str(obj[0])
 		print "============"
-x = PPS()
-x.insertCharacter('a',1)
-x.print_pps()
-x.insertCharacter('b',2)
-x.print_pps()
-x.insertCharacter('c',3)
-x.print_pps()
-x.insertCharacter('d',4)
-x.print_pps()
-x.insertCharacter('f',2)
-x.print_pps()
-x.deleteCharacter('b',3)
-x.print_pps()
-x.insertCharacter('f',3)
-x.print_pps()
+	def get_pps(self):
+		return self.ppsList
+	def clear(self):
+		del self.ppsList[:]
+	def constructPPSFromDB(self,string):
+		self.clear()
+		for p,c in enumerate(string):
+			self.insertCharacter(c,p+1)
+
+# x = PPS()
+# x.insertCharacter('a',1)
+# x.print_pps()
+# x.insertCharacter('b',2)
+# x.print_pps()
+# x.insertCharacter('c',3)
+# x.print_pps()
+# x.insertCharacter('d',4)
+# x.print_pps()
+# x.insertCharacter('f',2)
+# x.print_pps()
+# x.deleteCharacter('b',3)
+# x.print_pps()
+# x.insertCharacter('f',3)
+# x.print_pps()
 
 
--- renderHTML.py
+++ renderHTML.py
@@ -1,14 +1,16 @@
 import flask 
 import redis
+from PPS import PPS
 app = flask.Flask(__name__)
 app.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 app.secret_key = 'asdf'
-
+pps = PPS()
 def listen():
     pubsub = app.redis.pubsub()
     pubsub.subscribe('collabEdit')
     for message in pubsub.listen():
-        yield 'data: %s\n\n' % message['data']
+    	if message["type"] != "subscribe":
+    		yield 'data: %s' % message['data']
 
 @app.route('/login', methods=["POST","GET"])
 def login():
@@ -20,19 +22,29 @@ def login():
 
 @app.route('/')
 def hello():
-    if 'user' not in flask.session:
-    	return flask.redirect('/login')
-    return flask.render_template('main.html')
+	pps.constructPPSFromDB(app.redis.get("content"))
+	if 'user' not in flask.session:
+		return flask.redirect('/login')
+	return flask.render_template('main.html',content=app.redis.get("content"))
 
 @app.route('/listen')
 def stream():
     return flask.Response(listen(),
                           mimetype="text/event-stream")
+@app.route('/getPPS')
+def getPPS():
+	return flask.jsonify(pps=pps.get_pps())
 
 @app.route("/data", methods=["POST","GET"])
 def data():
 	if flask.request.method == "POST":
 		app.redis.set("content", flask.request.form.get("content"))
+		position = int(flask.request.form.get("position"))
+		operation_type = int(flask.request.form.get("type"))
+		if operation_type == 1:
+		 	pps.deleteCharacter(flask.request.form.get("character"),position)
+		else:
+			pps.insertCharacter(flask.request.form.get("character"),position)
 		app.redis.publish("collabEdit",flask.request.form.get("content"))
 		return flask.jsonify(success=True)
 	elif flask.request.method == "GET":
@@ -40,4 +52,5 @@ def data():
 
 if __name__ == "__main__":
 	app.debug=True
-	app.run(host= '0.0.0.0',threaded=True) 
+	#app.run(host= '0.0.0.0',threaded=True)
+	app.run(threaded=True) 
